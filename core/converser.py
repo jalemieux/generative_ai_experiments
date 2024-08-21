86d2242fa610b1dc46a06514595da50e4e2bf73f
Jac Lemieux <jac@jacs-air.lan>
2024-08-20 22:30:27 -0700

    added code reviewer

--- core/converser.py
+++ core/converser.py
@@ -2,11 +2,14 @@ from typing import Dict, List, Tuple, Any, Type
 from pydantic import BaseModel, Field
 from core.actor import Actor
 from core.completion import Completion, OpenAICompletion
-
+import json
 
 class Message(BaseModel):
     role: str # system, user, assistant
     content: str
+
+    def __str__(self):
+        return f"{self.role}: {self.content}"
 
     def out(self):
         return { "role": self.role, "content": self.content }
@@ -26,39 +29,51 @@ class SystemMessage(Message):
         super().__init__(role="system", content=content)
 
 class Converser:
-    examples: Dict[str, str] = {}
-    messages: List[Message] = []
+    examples: Dict[str, str]
+    messages: List[Message]
     completion: Completion
-    actors: List[Actor] = []
+    actors: List[Actor]
+    persist: str
 
-    def __init__(self, completion, instruction,
+    def __init__(self, completion, instruction=None,
+                 persist=None,
                  examples: Dict[str,str]=None,
                  actors: List[Actor]=None):
         self.completion = completion
-        self.messages.append(SystemMessage(instruction))
+        self.persist = persist
+        self.messages = []
+        if instruction is not None:
+            self._add_message(SystemMessage(instruction))
         if examples is not None:
             self.examples = examples
         if actors is not None:
             self.actors = actors
 
+    def _add_message(self, msg: Message):
+        self.messages.append(msg)
+        if self.persist is not None:
+            self.save(self.persist)
+
+
     def response(self, str: str, response_obj:Type[BaseModel] =None,) -> str:
-        self.messages.append(UserMessage(content=str))
+        self._add_message(UserMessage(content=str))
         if response_obj is not None:
             resp = self.completion.parse(messages=[msg.out() for msg in self.messages], response_format=response_obj)
-            self.messages.append(AssistantMessage(content=resp.json()))
+            self._add_message(AssistantMessage(content=resp.json()))
 
         else:
             resp = self.completion.complete(messages=[msg.out() for msg in self.messages])
-            self.messages.append(AssistantMessage(content=resp))
+            self._add_message(AssistantMessage(content=resp))
 
         return resp
 
     def tool(self, str: str) -> tuple[Actor, Any] | None:
-        self.messages.append(UserMessage(content=str))
+        self._add_message(UserMessage(content=str))
         fcts = self.completion.force_tool(
             messages=[msg.out() for msg in self.messages],
             tools=[actor.tool() for actor in self.actors])
 
+        self._add_message(AssistantMessage(content=fcts))
         for actor in self.actors:
             for fct in fcts:
                 if fct["name"] == actor.name:
@@ -72,7 +87,19 @@ class Converser:
         else:
             return None
 
-# completion = OpenAICompletion()
-# c = Converser(completion=completion, instruction = "you are a helpful assistant")
-# resp = c.response("what color is the sea")
-# print(c.messages)
+    def dump(self, fp):
+        with open(fp, 'w') as f:
+            f.write("\n".join([ str(m) for m in self.messages]))
+
+    def save(self, fp):
+        with open(fp, 'w') as f:
+            json.dump([message.dict() for message in self.messages], f, indent=4)
+
+    @staticmethod
+    def load_messages(fp) -> List[Message]:
+        with open(fp, 'r') as f:
+            data = json.load(f)
+            return [Message(**m) for m in data]
+
+
+
--- reflection/code_review.py
+++ reflection/code_review.py
@@ -19,30 +19,50 @@ class AICode(BaseModel):
 class Bug(BaseModel):
     python_code: str
     explanation: str
+    def __str__(self):
+        return f"```{self.python_code}```{self.explanation}\n"
 class CodeReview(BaseModel):
     bugs: List[Bug]
     """summary of bugs"""
     review_comments: str = Field(description="summary of bugs")
-    accepted: bool
+    """true if the code is ok, false if there is at least 1 bug"""
+    accepted: bool = Field(description="true if the code is ok, false if there is at least 1 bug")
 
 client = OpenAICompletion()
-# Bob is the dev
-bob = Converser(client, instruction="write python code that minimize the number of tokens in a string")
+#Bob is the dev
+bob = Converser(client, instruction="write python code that minimize the number of tokens in a string",
+                persist="./bob.json")
 bob_code = bob.response("", response_obj=AICode) # python_code, explanation
 
-fred = Converser(client,instruction= "Write python code that implements the provided requirements: ")
+# # Fred is the reviewer
+fred = Converser(client,instruction= "Write python code that implements the provided requirements: ",
+                 persist="./fred.json")
 fred_code = fred.response("requirements: minimize the number of tokens in a string", response_obj=AICode)
 
-# Fred is the reviewer
 fred_review = fred.response(
     f"compare your code with the code provided and evaluate if the code provided has any bugs. Code:{{code}}".format(code=bob_code.python_code),
     response_obj=CodeReview
     )
+# back to Bob for code fix
+feedback_to_bob = f"""
+Evaluate the following code review feedback and fix your code accordingly.
+Code Review: {{comments}}
+Bugs (if any): 
+{{bugs}}
+Code passed review: {{status}}
+""".format(
+    comments=fred_review.review_comments,
+    bugs="\n".join([str(bug) for bug in fred_review.bugs]),
+    status=fred_review.accepted
+)
 
-# Frank is the manager
-frank = Converser(client, instruction="Review the provided code and code review and evaluate if the review is correct.")
-frank_resp = frank.response(f"code: ```{bob_code.python_code}```, code review: {fred_review.json()}")
-print(frank.last_message())
+bob.response(feedback_to_bob)
+
+
+# # Frank is the reviewer's reviewer
+# frank = Converser(client, instruction="you are given code, and a code review. your task is evaluate if the review correctly accepts or reject the code.")
+# frank_resp = frank.response(f"code: ```{bob_code.python_code}```, code review: bugs: {{bugs}}, {fred_review.json()}.")
+
 
 
 
